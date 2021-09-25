7c47a79025c2ff5d3a43f45243ada31b178081d9
uzma maryam <uzmamaryam48@gmail.com>
2021-09-25 14:00:02 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,66 @@
+   //write a program which shows the couter after each second.
+
+// function clock(){
+//     var date = new Date();
+// var hrs = date.getHours();
+// var min = date.getMinutes();
+// var sec = date.getSeconds();
+//         console.log(`current time : ${hrs} ${min} ${sec}`);
+// }
+// setInterval(clock,1000)
+
+
+
+
+    // // 2. write a function which takes your name and displays the greeting with your name
+    // function fullname() {
+    //     firstname= "uzma"
+    //     lastname ="maryam"
+ 
+    //     console.log(`hellow ${firstname} ${lastname} `);
+        
+    // }
+    // fullname()
+
+
+    // //3.write a function(arrow function) which takes two values and return its sum as a result
+
+
+//     let sum = (c, d) => {  
+  
+//  let result = c + d;
+
+//   return result; 
+// };
+// console.log( sum(7, 2)); 
+
+
+    // //4. write a function(arrow function) which takes a number and multiply it with 0.5 and return the new value. print the new value outside the function
+//     const array1 = [4, 2, 9, 10];
+
+// const map1 = array1.map(x => x * 0.5);
+
+// console.log(map1);
+
+
+
+
+// //5. print simple array of [1,2,3,4,5] with the help of array map funtion
+// let arr =[1,2,3,4,5]
+// arr.map((arr) => console.log(arr))
+
+
+
+// 6. let arr = [{id:1,name:"abc"},{id:1,name:"efg"},{id:2,name:"hij"},{id:3,name:"xyz"}] iterate the given array through map function and print the name and id
+// let records = [
+//     {id :101, name : 'momina'},
+//     {id :102, name : 'noor'},
+//     {id :103, name : 'kiran'}
+
+// ]
+// records.map ((a) =>{
+//     console.log(`id: ${a.id} name: ${a.name} `);
+// })
+
+
+
